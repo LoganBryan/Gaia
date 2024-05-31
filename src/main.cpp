0e7245d2b44399320e408ead175b0098c3cf95aa
Logan Bryan <loganbryan234@gmail.com>
2024-05-31 17:20:59 +0100

    Commented out GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
    Caused memory issues

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
+//#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
